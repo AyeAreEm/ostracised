522de078fbe738a0873e046c5f9b875a1ea6e658
AyeAreEm <ag.ayeareem@gmail.com>
2024-07-23 19:27:39 +0800

    tweak movement

--- player/player.gd
+++ player/player.gd
@@ -1,29 +1,39 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var SPEED = 150
+@export var ACCELERATION = 10
+@export var FRICTION = 80
+@export var JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	$Sprite2D/AnimationPlayer.play("Idle_temp")
+	
+func apply_friction():
+	velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
 func _physics_process(delta):
-	# Add the gravity.
+	# this quits the game, will change to be the menu
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	
+	# Add the gravity
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		velocity.x = direction * SPEED
+		#velocity.x = direction * SPEED
+		velocity = velocity.move_toward(Vector2(direction * SPEED, 0), ACCELERATION)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		apply_friction()
+		#velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
