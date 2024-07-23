22c0c65416225f55d56a00c521a985903708c4cf
AyeAreEm <ag.ayeareem@gmail.com>
2024-07-23 20:48:15 +0800

    add sprint, fix movement, attach cam

--- player/player.gd
+++ player/player.gd
@@ -1,39 +1,46 @@
 extends CharacterBody2D
 
-@export var SPEED = 150
-@export var ACCELERATION = 10
-@export var FRICTION = 80
-@export var JUMP_VELOCITY = -300.0
+@export var WALK_SPEED = 80
+@export var RUN_SPEED = 130
+@export var FRICTION = 100
+@export var ACCELERATION = 50
+@export var JUMP_VELOCITY = -220
+
+var CURRENT_SPEED = WALK_SPEED
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	$Sprite2D/AnimationPlayer.play("Idle_temp")
-	
-func apply_friction():
-	velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 
 func _physics_process(delta):
+	if Input.is_action_pressed("sprint"):
+		CURRENT_SPEED = RUN_SPEED
+	else:
+		CURRENT_SPEED = WALK_SPEED
+	
 	# this quits the game, will change to be the menu
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-	
-	# Add the gravity
-	if not is_on_floor():
-		velocity.y += gravity * delta
 
 	# Handle jump
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
+	# -1 means left, 1 means right
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
-		#velocity.x = direction * SPEED
-		velocity = velocity.move_toward(Vector2(direction * SPEED, 0), ACCELERATION)
+		if direction == -1:
+			pass
+		else:
+			pass
+		velocity.x = velocity.move_toward(Vector2(direction * CURRENT_SPEED, global_transform.origin.y), ACCELERATION).x
 	else:
-		apply_friction()
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		
+	# Add the gravity
+	if not is_on_floor():
+		velocity.y += gravity * delta
 
 	move_and_slide()
