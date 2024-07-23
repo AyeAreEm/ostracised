36f65eb345f03bd7d580c2fd5e7584321c5abbe9
AyeAreEm <ag.ayeareem@gmail.com>
2024-07-23 21:35:27 +0800

    start charge jump

--- player/player.gd
+++ player/player.gd
@@ -7,12 +7,29 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY = -220
 
 var CURRENT_SPEED = WALK_SPEED
+var jump_start = 0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready():
 	$Sprite2D/AnimationPlayer.play("Idle_temp")
+	
+func handle_jump():
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		jump_start = Time.get_unix_time_from_system()
+		
+	if Input.is_action_just_released("ui_accept") and is_on_floor():
+		var time_elapsed = Time.get_unix_time_from_system() - jump_start
+		print(time_elapsed)
+		
+		if time_elapsed > 1.5:
+			velocity.y = JUMP_VELOCITY * 2
+		else:
+			if JUMP_VELOCITY * time_elapsed < JUMP_VELOCITY * -1:
+				velocity.y = JUMP_VELOCITY
+			else:
+				velocity.y = JUMP_VELOCITY * time_elapsed
 
 func _physics_process(delta):
 	if Input.is_action_pressed("sprint"):
@@ -25,8 +42,7 @@ func _physics_process(delta):
 		get_tree().quit()
 
 	# Handle jump
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	handle_jump()
 
 	# -1 means left, 1 means right
 	var direction = Input.get_axis("ui_left", "ui_right")
