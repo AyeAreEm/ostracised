6c2946ccaca8793a84284edfe8e54be10fab3130
AyeAreEm <ag.ayeareem@gmail.com>
2024-07-26 16:16:50 +0800

    enemy attack w/ cooldown

--- enemies/placeholder/Enemy_Placeholder.gd
+++ enemies/placeholder/Enemy_Placeholder.gd
@@ -1,7 +1,21 @@
 extends CharacterBody2D
 
 @export var player_stats: Stats
+var player: CharacterBody2D
+var start_time = 0
 
 func _ready():
 	$EnemySprite/AnimationPlayer.play("enemy_idle")
-	player_stats.take_damage(20)
+	player = get_tree().get_first_node_in_group("Player")
+
+func _physics_process(delta):
+	var current_time = Time.get_unix_time_from_system()
+	var direction = player.global_position - global_position
+	
+	if direction.length() <= 50:
+		if start_time == 0:
+			start_time = Time.get_unix_time_from_system()
+		elif current_time - start_time >= 1:
+			player_stats.take_damage(10)
+			start_time = 0
+			
