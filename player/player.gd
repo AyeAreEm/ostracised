b8502b7020db49b6a0e48445e7c8ad8ec3273b84
AyeAreEm <ag.ayeareem@gmail.com>
2024-07-26 21:36:40 +0800

    play walk anim, jump not work?

--- player/player.gd
+++ player/player.gd
@@ -11,6 +11,8 @@ extends CharacterBody2D
 #makes it so we dont have to get it every time 
 @onready var AnimPlay = $AnimationSpriteSheet/AnimationPlayer
 @onready var Sprite = $AnimationSpriteSheet
+@onready var PauseMenu = $Camera2D/PauseMenu
+@onready var HealthBar = $Camera2D/HealthBar
 
 var CURRENT_SPEED = WALK_SPEED
 var paused = false
@@ -25,7 +27,7 @@ func _ready():
 	
 func handle_damage(damage):
 	print("this is the remaining health: ", player_stats.health)
-	$Camera2D/HealthBar.position.x -= damage
+	HealthBar.position.x -= damage
 	
 func handle_death():
 	print("you died")
@@ -36,10 +38,10 @@ func handle_roll():
 		
 func handle_pause_menu():
 	if paused:
-		$Camera2D/PauseMenu.hide()
+		PauseMenu.hide()
 		Engine.time_scale = 1
 	else:
-		$Camera2D/PauseMenu.show()
+		PauseMenu.show()
 		Engine.time_scale = 0
 		
 	paused = !paused
@@ -68,6 +70,8 @@ func _physics_process(delta):
 	# Handle jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		print("playing jump")
+		AnimPlay.play("kingsguard_jump")
 
 	# -1 means left, 1 means right
 	var direction = Input.get_axis("left", "right")
@@ -75,14 +79,15 @@ func _physics_process(delta):
 	if direction:
 		if direction == -1:
 			Sprite.flip_h = true
-			#AnimPlay.play("kingsguard_walk")
+			AnimPlay.play("kingsguard_walk")
 		else:
 			Sprite.flip_h = false
-			#AnimPlay.play("kingsguard_walk")
+			AnimPlay.play("kingsguard_walk")
 		velocity.x = velocity.move_toward(Vector2(direction * CURRENT_SPEED, global_transform.origin.y), ACCELERATION).x
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, FRICTION)
+		AnimPlay.play("kingsguard_idle")
 		
 	# Add the gravity
 	if not is_on_floor():
